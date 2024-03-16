6f4f7410c0d3cf013cac181382d81373d19df871
Charles Choi <kickingvegas@gmail.com>
2024-03-15 22:50:27 -0700

    Bump version to 1.3.0

--- cc-isearch-menu.el
+++ cc-isearch-menu.el
@@ -5,7 +5,7 @@
 ;; Author: Charles Choi <kickingvegas@gmail.com>
 ;; URL: https://github.com/kickingvegas/cc-isearch-menu
 ;; Keywords: wp
-;; Version: 1.2.1
+;; Version: 1.3.0
 ;; Package-Requires: ((emacs "29.1"))
 ;;
 

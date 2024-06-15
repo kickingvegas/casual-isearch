bff7137193b7a65ccb8888a0897b9bd615015b0f
Charles Choi <kickingvegas@gmail.com>
2024-06-15 09:30:43 -0700

    Bump version to 1.6.0

--- casual-isearch.el
+++ casual-isearch.el
@@ -5,7 +5,7 @@
 ;; Author: Charles Choi <kickingvegas@gmail.com>
 ;; URL: https://github.com/kickingvegas/casual-isearch
 ;; Keywords: wp
-;; Version: 1.5.0
+;; Version: 1.6.0
 ;; Package-Requires: ((emacs "29.1"))
 ;;
 
